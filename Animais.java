53337ac75be17aeaacd341297d3f3a4b779b84c4
Tobias Sousa <100934496+tobiassousa@users.noreply.github.com>
2022-09-12 20:16:21 -0300

    Add files via upload

---
+++ Animais.java
@@ -0,0 +1,44 @@
+package com.mycompany.animals;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TI ADCe
+ */
+
+
+public class Animais {
+    private String nome;
+    private String peso;
+    private String idade;
+    private String sexo;
+
+public void setNome(String nome){
+    this.nome = nome;
+}
+public String getNome(){
+    return this.nome;
+}
+public void setPeso(String peso){
+    this.peso = peso;
+}
+public String getPeso(){
+    return this.peso;
+}
+public void setIdade(String idade){
+    this.idade = idade;
+}
+public String getIdade(){
+    return this.idade;
+}
+public void setSexo(String sexo){
+    this.sexo =sexo;
+}
+public String getSexo(){
+    return this.sexo;
+}
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Animais
+![Imagem1](https://user-images.githubusercontent.com/100934496/189509031-df40b3f7-9c5f-45ab-a0c8-add5be44821d.jpg)
+
+
+## Atributos
+- Nome
+- Peso
+- Idade
+
+## Tela de cadastro 
+![Imagem2](../../Capturar.PNG)
+
+## Dados gravados em CSV
+![Iamgem3](../../a.PNG)
